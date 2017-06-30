45d184fe8759a33cda7dd32fffe28759ad75eca2
hon.trang <trang11392@gmail.com>
2017-06-30 23:02:55 +0700

    move override methods to views.py instead of serializers.py to better handle
    Signed-off-by: hon.trang <trang11392@gmail.com>

--- backend/webapp/serializers.py
+++ backend/webapp/serializers.py
@@ -12,37 +12,6 @@ class ProductSerializer(serializers.DocumentSerializer):
     """
     TBD
     """
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        filesrc = validated_data['imageSource']
-        validated_data['imageUrl'] = self.saveFileLocal(filesrc)
-        return Products.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.created = validated_data.get('created', instance.created)
-        instance.imageSource = validated_data.get('imageSource', instance.imageSource)
-        instance.imageUrl = self.saveFileLocal(validated_data['imageSource'])
-        instance.save()
-        return instance
-    def saveFileLocal(self, source):
-        filesrc = source
-        ch = filesrc.chunks()
-        filename = "%s-%s" %(str(time.time()).replace('.','_'), filesrc.name)
-        with open('%s/%s' %(settings.STATICFILES_DIRS, filename ), 'w+b') as file:
-            for d in ch:
-                file.write(d)
-            file.close()
-        return filename
-    
-    def deleteExistedLocal(self, source):
-        pass
-
     class Meta:
         """
         TBD
--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,8 +1,9 @@
 import os
 
 from rest_framework_mongoengine import viewsets
-from .models import Products,Users
-from .serializers import ProductSerializer,UserSerializer
+from .models import Products, Users
+from .utils import FileHandle
+from .serializers import ProductSerializer, UserSerializer
 from PIL import Image
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +14,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.http import HttpResponse
 from rest_framework.parsers import JSONParser
+
 
 @csrf_exempt
 def upload_file(request):
@@ -32,15 +34,55 @@ def upload_file(request):
         html = "<html><body>%s</body></html>" % _form
         return HttpResponse(html)
 
+
 def handle_uploaded_file(f):
     with open('./backend/static/test.txt', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'id'
+    """
+    Create a model instance.
+    """
+
+    def create(self, request, *args, **kwargs):
+        request.data['imageUrl'] = FileHandle.saveFileLocal(
+            self, request.data['imageSource'])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Update a model instance.
+        """
+        request.data['imageUrl'] = FileHandle.saveFileLocal(
+            self, request.data['imageSource'])
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        FileHandle.deleteExistedLocal(self,instance['imageUrl'])
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        FileHandle.deleteExistedLocal(self,instance['imageUrl'])
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all()
