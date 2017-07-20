bf10ced36ca34279d841d76c786da9894c84d867
hon.trang <trang11392@gmail.com>
2017-07-20 23:32:26 +0700

    lack code raise_errors_on_nested_writes

--- backend/webapp/serializers.py
+++ backend/webapp/serializers.py
@@ -10,6 +10,40 @@ from mongoengine.errors import ValidationError as me_ValidationError
 logger = logging.getLogger(__name__)
 
 
+def raise_errors_on_nested_writes(method_name, serializer, validated_data):
+    # *** inherited from DRF 3, altered for EmbeddedDocumentSerializer to pass ***
+    assert not any(
+        isinstance(field, serializers.DocumentSerializer) and
+        not isinstance(field, serializers.EmbeddedDocumentSerializer) and
+        (key in validated_data)
+        for key, field in serializer.fields.items()
+    ), (
+        'The `.{method_name}()` method does not support writable nested'
+        'fields by default.\nWrite an explicit `.{method_name}()` method for '
+        'serializer `{module}.{class_name}`, or set `read_only=True` on '
+        'nested serializer fields.'.format(
+            method_name=method_name,
+            module=serializer.__class__.__module__,
+            class_name=serializer.__class__.__name__
+        )
+    )
+
+    assert not any(
+        '.' in field.source and (key in validated_data) and
+        isinstance(validated_data[key], (list, dict))
+        for key, field in serializer.fields.items()
+    ), (
+        'The `.{method_name}()` method does not support writable dotted-source '
+        'fields by default.\nWrite an explicit `.{method_name}()` method for '
+        'serializer `{module}.{class_name}`, or set `read_only=True` on '
+        'dotted-source serializer fields.'.format(
+            method_name=method_name,
+            module=serializer.__class__.__module__,
+            class_name=serializer.__class__.__name__
+        )
+    )
+
+
 class BaseAppSerializer(serializers.DocumentSerializer):
     """define base serializer
     """
